46e89ec7161a71fc1f88edda4ac38dab269c4a26
Warui Ngugi <waruingugientp@gmail.com>
2020-04-16 00:43:53 +0300

    Added project root path in settings

--- corona/settings.py
+++ corona/settings.py
@@ -110,6 +110,11 @@ USE_L10N = True
 
 USE_TZ = False
 
+# HEROKU SETTINGS
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_URL = '/static/'
